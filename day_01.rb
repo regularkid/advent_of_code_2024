835d86f6fea261a99fb6dae1d00f821298ab03db
Bryan Perfetto <regularkid@gmail.com>
2024-12-01 16:51:26 -0800

    Day 01 - Part 1

--- day_01.rb
+++ day_01.rb
@@ -1,1 +1,18 @@
-puts("Hello World!")
+leftList = []
+rightList = []
+
+File.readlines("day_01_input.txt").each do |line|
+    nums = line.split
+    leftList.push(nums[0])
+    rightList.push(nums[1])
+end
+
+leftList.sort!
+rightList.sort!
+
+totalDelta = 0
+(0...leftList.length).each do
+    |i| totalDelta += (Integer(leftList[i]) - Integer(rightList[i])).abs
+end
+
+puts totalDelta
