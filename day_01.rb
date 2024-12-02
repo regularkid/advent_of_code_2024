1c853f43e75df1e7dd04a79cb50337f6b05e7e31
Bryan Perfetto <regularkid@gmail.com>
2024-12-01 17:15:56 -0800

    Added comments.

--- day_01.rb
+++ day_01.rb
@@ -1,25 +1,30 @@
+# Parse input
 leftList = []
 rightList = []
-
 File.readlines("day_01_input.txt").each do |line|
     nums = line.split
     leftList.push(nums[0])
     rightList.push(nums[1])
 end
 
+# Sort in place ('!' is convention for 'modifies variable rather than returning a copy')
 leftList.sort!
 rightList.sort!
 
+# Calculate absolute delta between each pair of values from the 2 lists
 totalDelta = 0
-(0...leftList.length).each do |i|
-    totalDelta += (Integer(leftList[i]) - Integer(rightList[i])).abs
+leftList.each_with_index do |num, index|
+    totalDelta += (Integer(num) - Integer(rightList[index])).abs
 end
 
 puts "Part 1 Answer: #{totalDelta}"
 
+# Count occurrence of each number in 2nd/right list
 similarityScore = 0
 rightListCounts = Hash.new(0)
 rightList.each { |num| rightListCounts[num] += 1 }
+
+# Add up {left list value} * {num occurrences of that value in 2nd/right list}
 leftList.each { |num| similarityScore += Integer(num) * rightListCounts[num]}
 
 puts "Part 2 Answer: #{similarityScore}"
