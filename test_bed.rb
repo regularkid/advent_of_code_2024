6a7025e53d4acbae9fbeb6dbfc7122df31d2928d
Bryan Perfetto <regularkid@gmail.com>
2024-12-02 18:15:50 -0800

    Added 'test_bed' file for playing around with things.

---
+++ test_bed.rb
@@ -0,0 +1,26 @@
+s = "1a"
+30.times do
+    puts s
+    s.next!
+end
+
+puts [1, 2, 3, 4, 5].sample
+
+def odd_or_even(numbers)
+    numbers.map do |number|
+        number.odd? ? "odd" : "even"
+    end
+end
+  
+puts odd_or_even([1,2,4,3]).to_s
+
+puts case (1..100).to_a.sample
+        when 0..33
+            "Bottom Third"
+        when 34..66
+            "Middle Third"
+        when 67..99
+            "Top Third"
+        else
+            "One Hundred"
+        end
