40d669b491a1758253297123561038da8690a2ca
Bryan Perfetto <regularkid@gmail.com>
2024-12-10 06:24:52 -0800

    Day 10 - Part 2

--- day_10.rb
+++ day_10.rb
@@ -1,5 +1,7 @@
 require 'set'
 
+RatingCount = Struct.new(:count)
+
 class Map
     def initialize(filename)
         @grid = []
@@ -31,6 +33,29 @@ class Map
         end
     end
 
+    def get_trailhead_rating(x, y, nextHeight, ratingCount)
+        if @grid[y][x] == 9
+            ratingCount.count += 1
+            return
+        end
+
+        if x > 0 && @grid[y][x - 1] == nextHeight
+            get_trailhead_rating(x - 1, y, nextHeight + 1, ratingCount)
+        end
+
+        if x < @grid[y].length - 1 && @grid[y][x + 1] == nextHeight
+            get_trailhead_rating(x + 1, y, nextHeight + 1, ratingCount)
+        end
+
+        if y > 0 && @grid[y - 1][x] == nextHeight
+            get_trailhead_rating(x, y - 1, nextHeight + 1, ratingCount)
+        end
+
+        if y < @grid.length - 1 && @grid[y + 1][x] == nextHeight
+            get_trailhead_rating(x, y + 1, nextHeight + 1, ratingCount)
+        end
+    end
+
     def solve_part_1
         trailheadScoreSum = 0
 
@@ -48,7 +73,27 @@ class Map
 
         puts "Part 1 Answer: #{trailheadScoreSum}"
     end
+
+    def solve_part_2
+        trailheadRatingSum = 0
+
+        @grid.length.times do |y|
+            @grid[y].length.times do |x|
+                if @grid[y][x] != 0
+                    next
+                end
+
+                trailheadRatingCount = RatingCount.new
+                trailheadRatingCount.count = 0
+                get_trailhead_rating(x, y, 1, trailheadRatingCount)
+                trailheadRatingSum += trailheadRatingCount.count
+            end
+        end
+
+        puts "Part 2 Answer: #{trailheadRatingSum}"
+    end
 end
 
 map = Map.new("day_10_input.txt")
 map.solve_part_1
+map.solve_part_2
