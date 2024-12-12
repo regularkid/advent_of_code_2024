0a09ce453f6560181a26f2ab2c7548db380c1cc7
Bryan Perfetto <regularkid@gmail.com>
2024-12-11 20:48:06 -0800

    Day 11 - Part 2

--- day_11.rb
+++ day_11.rb
@@ -40,8 +40,49 @@ class Stones
         end
         puts "Part 2 Answer: #{@stones.length}"
     end
+
+    def blink_recursive(number, blinkIteration)
+        if blinkIteration == 75
+            return 1
+        end
+
+        memoId = "#{number},#{blinkIteration}"
+        if @memo.key?(memoId)
+            return @memo[memoId]
+        end
+
+        numStones = 0
+
+        if number == 0
+            numStones += blink_recursive(1, blinkIteration + 1)
+        elsif number.to_s.length % 2 == 0
+            s = number.to_s
+            left = s[0...s.length/2]
+            right = s[s.length/2..]
+            numStones += blink_recursive(left.to_i, blinkIteration + 1)
+            numStones += blink_recursive(right.to_i, blinkIteration + 1)
+        else
+            numStones += blink_recursive(number * 2024, blinkIteration + 1)
+        end
+
+        @memo[memoId] = numStones
+        return numStones
+    end
+
+    def solve_part_2_recursive
+        @memo = Hash.new
+       
+        numStones = 0
+        @stones.each do |number|
+            numStones += blink_recursive(number, 0)
+        end
+        puts "Part 2 Answer: #{numStones}"
+    end
 end
 
 stones = Stones.new("day_11_input.txt")
 stones.solve_part_1
-stones.solve_part_2
+
+stones2 = Stones.new("day_11_input.txt")
+#stones2.solve_part_2
+stones2.solve_part_2_recursive
