59dde7380498043e26af56b167dd004cde476bc9
Bryan Perfetto <regularkid@gmail.com>
2024-12-13 05:00:54 -0800

    Day 12 - Part 2

--- day_12.rb
+++ day_12.rb
@@ -1,6 +1,6 @@
 require 'set'
 
-RegionInfo = Struct.new(:area, :permiter)
+RegionInfo = Struct.new(:area, :perimeter, :corners)
 
 class Garden
     def initialize(filename)
@@ -14,19 +14,22 @@ class Garden
         @visited = Set.new
 
         priceSum = 0
+        priceSum2 = 0
         (0...@grid.length).each do |y|
             (0...@grid[y].length).each do |x|
                 if !@visited.include?(get_plot_id(x, y))
-                    regionInfo = RegionInfo.new(0, 0)
+                    regionInfo = RegionInfo.new(0, 0, 0)
                     visit_plot(x, y, regionInfo)
-                    price = regionInfo.area * regionInfo.permiter
+                    price = regionInfo.area * regionInfo.perimeter
+                    price2 = regionInfo.area * regionInfo.corners
                     priceSum += price
-                    puts "A region of #{@grid[y][x]} plants with price #{regionInfo.area} * #{regionInfo.permiter} = #{price}"
+                    priceSum2 += price2
+                    puts "A region of #{@grid[y][x]} plants with price #{regionInfo.area} * #{regionInfo.perimeter} and #{regionInfo.corners} = #{price}"
                 end
             end
         end
 
-        puts "Total price of garden is #{priceSum}"
+        puts "Total price of garden is #{priceSum} (part 1) and #{priceSum2} (part 2)"
     end
 
     def visit_plot(x, y, regionInfo)
@@ -43,28 +46,29 @@ class Garden
         type = @grid[y][x]
         regionInfo.area += 1
 
-        permiter = 4
+        perimeter = 4
         if plot_contains_type(x - 1, y, type)
-            permiter -= 1
+            perimeter -= 1
             visit_plot(x - 1, y, regionInfo)
         end
 
         if plot_contains_type(x + 1, y, type)
-            permiter -= 1
+            perimeter -= 1
             visit_plot(x + 1, y, regionInfo)
         end
 
         if plot_contains_type(x, y - 1, type)
-            permiter -= 1
+            perimeter -= 1
             visit_plot(x, y - 1, regionInfo)
         end
 
         if plot_contains_type(x, y + 1, type)
-            permiter -= 1
+            perimeter -= 1
             visit_plot(x, y + 1, regionInfo)
         end
 
-        regionInfo.permiter += permiter
+        regionInfo.perimeter += perimeter
+        regionInfo.corners += get_corner_count(x, y, perimeter)
     end
 
     def plot_contains_type(x, y, type)
@@ -81,6 +85,60 @@ class Garden
 
     def is_within_garden(x, y)
         return x >= 0 && x < @grid[0].length && y >= 0 && y < @grid.length
+    end
+
+    def get_corner_count(x, y, perimeter)
+        # Single isolated plot
+        if perimeter == 4
+            return 4
+        end
+
+        # End cap
+        if perimeter == 3
+            return 2
+        end
+
+        type = @grid[y][x]
+        leftMatches = plot_contains_type(x - 1, y, type)
+        rightMatches = plot_contains_type(x + 1, y, type)
+        topMatches = plot_contains_type(x, y - 1, type)
+        bottomMatches = plot_contains_type(x, y + 1, type)
+
+        tlMatches = plot_contains_type(x - 1, y - 1, type)
+        trMatches = plot_contains_type(x + 1, y - 1, type)
+        blMatches = plot_contains_type(x - 1, y + 1, type)
+        brMatches = plot_contains_type(x + 1, y + 1, type)
+
+        cornerCount = 0
+
+        # Outside corner
+        if perimeter == 2
+            if (leftMatches && topMatches) ||
+               (leftMatches && bottomMatches) ||
+               (rightMatches && topMatches) ||
+               (rightMatches && bottomMatches)
+                cornerCount += 1
+            end
+        end
+
+        # Inside corners
+        if leftMatches && topMatches && !tlMatches
+            cornerCount += 1
+        end
+
+        if leftMatches && bottomMatches && !blMatches
+            cornerCount += 1
+        end
+
+        if rightMatches && topMatches && !trMatches
+            cornerCount += 1
+        end
+
+        if rightMatches && bottomMatches && !brMatches
+           cornerCount += 1
+        end
+
+        return cornerCount
     end
 end
 
