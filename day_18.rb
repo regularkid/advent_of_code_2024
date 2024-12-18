43ad6e9e11b07347cfb93cbd6a41c2977a8bd7a4
Bryan Perfetto <regularkid@gmail.com>
2024-12-18 08:30:58 -0800

    Day 18 - Part 2

--- day_18.rb
+++ day_18.rb
@@ -26,20 +26,22 @@ end
 class MemorySpace
     def initialize(filename, gridSize, numBytesToRead)
         @grid = []
+        @next_corruptions = []
         gridSize.times { @grid.append([]) }
         @grid.each do |row|
             gridSize.times { row.append(0) }
         end
 
         File.readlines(filename).each_with_index do |byte, index|
-            if index >= numBytesToRead
-                break
-            end
-
             values = byte.delete("\n").split(",").map(&:to_i)
             x = values[0]
             y = values[1]
-            @grid[y][x] = 1
+
+            if index < numBytesToRead
+                @grid[y][x] = 1
+            else
+                @next_corruptions.append(Pos.new(x, y))
+            end
         end
     end
 
@@ -89,6 +91,44 @@ class MemorySpace
         puts "Part 1 Answer: #{final_cost}"
     end
 
+    def solve_part_2(next_corruptions_to_add)
+        next_corruptions_to_add.times do |num|
+            next_corruption = @next_corruptions.shift
+            @grid[next_corruption.y][next_corruption.x] = 1
+        end
+
+        while !@next_corruptions.empty?
+            next_corruption = @next_corruptions.shift
+            @grid[next_corruption.y][next_corruption.x] = 1
+
+            frontier = PriorityQueue.new
+            came_from = Hash.new
+            cost_so_far = Hash.new
+
+            frontier.push(Pos.new(0, 0), 0)
+            cost_so_far[Pos.new(0, 0)] = 0
+
+            while !frontier.empty?
+                current = frontier.pop
+
+                get_neighbors(current).each do |neighbor|
+                    new_cost = cost_so_far[current] + 1
+                    if !cost_so_far.key?(neighbor) || new_cost < cost_so_far[neighbor]
+                        frontier.push(neighbor, new_cost)
+                        came_from[neighbor] = current
+                        cost_so_far[neighbor] = new_cost
+                    end
+                end
+            end
+
+            has_path = cost_so_far.key?(Pos.new(70, 70))
+            if !has_path
+                puts "No path once corruption #{next_corruption.x},#{next_corruption.y} is placed"
+                break
+            end
+        end
+    end
+
     def is_valid_pos(pos)
         return pos.x >= 0 && pos.x < @grid[0].length && pos.y >= 0 && pos.y < @grid.length && @grid[pos.y][pos.x] != 1
     end
@@ -108,4 +148,4 @@ class MemorySpace
 end
 
 memorySpace = MemorySpace.new("day_18_input.txt", 71, 1024)
-memorySpace.solve_part_1
+memorySpace.solve_part_2(1400)
